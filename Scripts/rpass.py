2b9ccc980e4531cd479daea42802adcbc59ada21
Sergey Semenov <sergey@vshome.net>
2021-09-05 19:49:07 +0300

    Create menu

--- Scripts/rpass.py
+++ Scripts/rpass.py
@@ -4,11 +4,9 @@ import subprocess
 import os
 import sys
 import time
-import configparser # For read config file
-import argparse     # For parse input argument almaz -h
+import configparser
+import argparse
 
-#from cmd import Cmd
-#from multiprocessing import Process, freeze_support
 # This is function only for pyinstaller
 def resource_path(relative_path):
  if getattr(sys, 'frozen', False):
@@ -20,8 +18,23 @@ def resource_path(relative_path):
 
 if __name__ == '__main__':
  parser = argparse.ArgumentParser(description='random pass')
- parser.add_argument("-s", dest="command", type=str, help='Support command config,....')
+ parser.add_argument("-a", metavar='<user>', dest="sys_user", type=str, help='Add user in system and create password')
+ parser.add_argument("-n", metavar='<user>', dest="user", type=str, help='Create password for user')
+ parser.add_argument("-q", metavar='<user>', dest="qr_user", type=str, help='Generate QR-code for user')
+ parser.add_argument("-c", metavar='<user>', dest="ch_user", type=str, help='Generate new hash for user')
+ parser.add_argument("-d", metavar='<user>', dest="del_user", type=str, help='Delete user')
  args = parser.parse_args()
+ if args.sys_user != None:
+  print ("Add user in system and create password")
+ if args.user != None:
+  print ("Create password for user")
+ if args.qr_user != None:
+  print ("Generate QR-code for user")
+ if args.ch_user != None:
+  print ("Generate new hash for user")
+ if args.del_user != None:
+  print ("Delete user")
+
 
 
 
